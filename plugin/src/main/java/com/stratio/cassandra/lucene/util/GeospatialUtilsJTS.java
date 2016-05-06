d74ffdea3c2455b450d3c0415b95dd69ab6c998d
Andrés De la Peña <a.penya.garcia@gmail.com>
2016-05-06 17:22:02 +0100

    Fix invalid WKT geometries by zero-buffering them.

--- plugin/src/main/java/com/stratio/cassandra/lucene/util/GeospatialUtilsJTS.java
+++ plugin/src/main/java/com/stratio/cassandra/lucene/util/GeospatialUtilsJTS.java
@@ -51,6 +51,9 @@ public class GeospatialUtilsJTS extends GeospatialUtils {
             GeometryFactory geometryFactory = context.getGeometryFactory();
             WKTReader reader = new WKTReader(geometryFactory);
             Geometry geometry = reader.read(string);
+            if (!geometry.isValid()) {
+                geometry = geometry.buffer(0);
+            }
             return context.makeShape(geometry);
         } catch (ParseException | IllegalArgumentException e) {
             throw new IndexException(e, "Shape '%s' is not parseable", string);
