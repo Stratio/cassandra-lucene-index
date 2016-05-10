e1b1e8837ef5ca587c75add057f58e9cade6a020
ealonsodb <eduardoalonso@stratio.com>
2016-05-10 15:34:30 +0200

    Added indexWriter.commit after indexWriter.deleteAll in Lucene Index.truncate

--- plugin/src/main/java/com/stratio/cassandra/lucene/service/LuceneIndex.java
+++ plugin/src/main/java/com/stratio/cassandra/lucene/service/LuceneIndex.java
@@ -168,6 +168,7 @@ public class LuceneIndex implements LuceneIndexMBean {
      */
     public void truncate() throws IOException {
         indexWriter.deleteAll();
+        indexWriter.commit();
         logger.info("{} truncated", name);
     }
 
