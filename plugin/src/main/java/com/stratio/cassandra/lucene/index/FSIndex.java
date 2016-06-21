1b31f75125fb60446b4feca39fb7b361abc01443
Andrés De la Peña <a.penya.garcia@gmail.com>
2016-06-22 00:32:18 +0100

    Fix logging

--- plugin/src/main/java/com/stratio/cassandra/lucene/index/FSIndex.java
+++ plugin/src/main/java/com/stratio/cassandra/lucene/index/FSIndex.java
@@ -239,7 +239,7 @@ public class FSIndex {
         try {
             close();
         } catch (Exception e) {
-            logger.error(String.format("Error deleting {}", name), e);
+            logger.error(String.format("Error deleting %s", name), e);
         } finally {
             FileUtils.deleteRecursive(path.toFile());
         }
@@ -256,6 +256,11 @@ public class FSIndex {
      * @return the found documents, sorted according to the supplied {@link Sort} instance
      */
     public DocumentIterator search(Query after, Query query, Sort sort, int count) {
+        logger.debug("Searching in {}\n" +
+                     "after: {}\n" +
+                     "query: {}\n" +
+                     " sort: {}\n" +
+                     "count: {}", name, after, query, sort, count);
         return new DocumentIterator(searcherManager, mergeSort, after, query, sort, count, fields);
     }
 
