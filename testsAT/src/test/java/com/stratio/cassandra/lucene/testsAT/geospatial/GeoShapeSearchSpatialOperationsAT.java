4a5539fd783533e01fe6b4b0446896bff5a0c9ee
Andrés De la Peña <a.penya.garcia@gmail.com>
2016-07-19 11:16:30 +0100

    Fix license header.

--- testsAT/src/test/java/com/stratio/cassandra/lucene/testsAT/geospatial/GeoShapeSearchSpatialOperationsAT.java
+++ testsAT/src/test/java/com/stratio/cassandra/lucene/testsAT/geospatial/GeoShapeSearchSpatialOperationsAT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Stratio (http:// stratio.com)
+ * Copyright (C) 2014 Stratio (http://stratio.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
