bf7b6cf8a1757de29f4ac45be502c5d36b3731bc
Eduardo Alonso <eduardoalonso@stratio.com>
2016-10-31 20:37:05 +0100

    Remove AT suites (#229)

---
+++ testsAT/src/test/java/com/stratio/cassandra/lucene/testsAT/varia/ReadStaticColumnsIT.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2014 Stratio (http://stratio.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.stratio.cassandra.lucene.testsAT.varia;
+
+import com.stratio.cassandra.lucene.testsAT.BaseIT;
+import com.stratio.cassandra.lucene.testsAT.util.CassandraUtils;
+import com.stratio.cassandra.lucene.testsAT.util.CassandraUtilsSelect;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static com.stratio.cassandra.lucene.builder.Builder.match;
+
+/**
+ * @author Eduardo Alonso {@literal <eduardoalonso@stratio.com>}
+ */
+public class ReadStaticColumnsIT extends BaseIT {
+
+    private static CassandraUtils utils;
+
+    @BeforeClass
+    public static void before() {
+        utils = CassandraUtils.builder("read_static_columns")
+                              .withPartitionKey("key")
+                              .withClusteringKey("cluster_key")
+                              .withColumn("key", "bigint")
+                              .withColumn("cluster_key", "int")
+                              .withStaticColumn("name", "text", false)
+                              .build()
+                              .createKeyspace()
+                              .createTable()
+                              .createIndex()
+                              .insert(new String[]{"key", "cluster_key", "name"},
+                                      new Object[]{12, 13, "Name12"})
+                              .insert(new String[]{"key", "cluster_key", "name"},
+                                      new Object[]{12, 14, "Name12-2"})
+                              .insert(new String[]{"key", "cluster_key", "name"},
+                                      new Object[]{15, 16, "Name15"})
+                              .insert(new String[]{"key", "cluster_key", "name"},
+                                      new Object[]{15, 17, "Name15-2"})
+                              .refresh();
+
+    }
+
+    @AfterClass
+    public static void after() {
+        utils.dropKeyspace();
+    }
+
+    @Test
+    public void testTokenReadStaticColumn1() {
+        CassandraUtilsSelect select = utils.filter(match("key", 12));
+        select.checkUnorderedColumns("key", 12L, 12L);
+        select.checkUnorderedColumns("cluster_key", 13, 14);
+        select.checkUnorderedColumns("name", "Name12-2", "Name12-2");
+    }
+
+    @Test
+    public void testTokenReadStaticColumn2() {
+        CassandraUtilsSelect select = utils.filter(match("key", 15));
+        select.checkUnorderedColumns("key", 15L, 15L);
+        select.checkUnorderedColumns("cluster_key", 16, 17);
+        select.checkUnorderedColumns("name", "Name15-2", "Name15-2");
+    }
+
+}
---
+++ testsAT/src/test/java/com/stratio/cassandra/lucene/testsAT/varia/TokenRangeWithSkinnyRowsIT.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (C) 2014 Stratio (http://stratio.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.stratio.cassandra.lucene.testsAT.varia;
+
+import com.stratio.cassandra.lucene.testsAT.BaseIT;
+import com.stratio.cassandra.lucene.testsAT.util.CassandraUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static com.stratio.cassandra.lucene.testsAT.varia.DataHelper.*;
+
+@RunWith(JUnit4.class)
+public class TokenRangeWithSkinnyRowsIT extends BaseIT {
+
+    private static CassandraUtils utils;
+
+    @BeforeClass
+    public static void before() {
+        utils = CassandraUtils.builder("token_range_skinny_rows")
+                              .withPartitionKey("integer_1", "ascii_1")
+                              .withColumn("ascii_1", "ascii")
+                              .withColumn("bigint_1", "bigint")
+                              .withColumn("blob_1", "blob")
+                              .withColumn("boolean_1", "boolean")
+                              .withColumn("decimal_1", "decimal")
+                              .withColumn("date_1", "timestamp")
+                              .withColumn("double_1", "double")
+                              .withColumn("float_1", "float")
+                              .withColumn("integer_1", "int")
+                              .withColumn("inet_1", "inet")
+                              .withColumn("text_1", "text")
+                              .withColumn("varchar_1", "varchar")
+                              .withColumn("uuid_1", "uuid")
+                              .withColumn("timeuuid_1", "timeuuid")
+                              .withColumn("list_1", "list<text>")
+                              .withColumn("set_1", "set<text>")
+                              .withColumn("map_1", "map<text,text>")
+                              .build()
+                              .createKeyspace()
+                              .createTable()
+                              .createIndex()
+                              .insert(data1,
+                                      data2,
+                                      data3,
+                                      data4,
+                                      data5,
+                                      data6,
+                                      data7,
+                                      data8,
+                                      data9,
+                                      data10,
+                                      data11,
+                                      data12,
+                                      data13,
+                                      data14,
+                                      data15,
+                                      data16,
+                                      data17,
+                                      data18,
+                                      data19,
+                                      data20)
+                              .refresh();
+    }
+
+    @AfterClass
+    public static void after() {
+        utils.dropIndex().dropTable().dropKeyspace();
+    }
+
+    @Test
+    public void testTokenSearch1() {
+        utils.searchAll().and("AND TOKEN(integer_1, ascii_1) > TOKEN(1, 'ascii')").check(4);
+    }
+
+    @Test
+    public void testTokenSearch2() {
+        utils.searchAll().and("AND TOKEN(integer_1, ascii_1) >= TOKEN(1, 'ascii')").check(5);
+    }
+
+    @Test
+    public void testTokenSearch3() {
+        utils.searchAll().and("AND TOKEN(integer_1, ascii_1) < TOKEN(1, 'ascii')").check(5);
+    }
+
+    @Test
+    public void testTokenSearch4() {
+        utils.searchAll().and("AND TOKEN(integer_1, ascii_1) <= TOKEN(1, 'ascii')").check(6);
+    }
+
+    @Test
+    public void testTokenSearch5() {
+        utils.searchAll()
+             .and("AND TOKEN(integer_1, ascii_1) > TOKEN(1, 'ascii')")
+             .and("AND TOKEN(integer_1, ascii_1) < TOKEN(3, 'ascii')")
+             .check(3);
+    }
+
+    @Test
+    public void testTokenSearch6() {
+        utils.searchAll()
+             .and("AND TOKEN(integer_1, ascii_1) >= TOKEN(1, 'ascii')")
+             .and("AND TOKEN(integer_1, ascii_1) < TOKEN(3, 'ascii')")
+             .check(4);
+    }
+
+    @Test
+    public void testTokenSearch7() {
+        utils.searchAll()
+             .and("AND TOKEN(integer_1, ascii_1) > TOKEN(1, 'ascii')")
+             .and("AND TOKEN(integer_1, ascii_1) <= TOKEN(3, 'ascii')")
+             .check(4);
+    }
+
+    @Test
+    public void testTokenSearch8() {
+        utils.searchAll()
+             .and("AND TOKEN(integer_1, ascii_1) >= TOKEN(1, 'ascii')")
+             .and("AND TOKEN(integer_1, ascii_1) <= TOKEN(3, 'ascii')")
+             .check(5);
+    }
+}
