6d01041b93797f1cbd7bda65de788652e65d87b2
Andrés de la Peña <adelapena@users.noreply.github.com>
2016-12-14 10:11:38 +0000

    Add support for local searches over index partitions with more than 2147483519 total documents

--- plugin/src/main/scala/com/stratio/cassandra/lucene/index/DocumentIterator.scala
+++ plugin/src/main/scala/com/stratio/cassandra/lucene/index/DocumentIterator.scala
@@ -27,19 +27,16 @@ import org.apache.lucene.search._
 
 /** [[CloseableIterator]] for retrieving Lucene documents satisfying a query.
   *
-  * @param searcher        the Lucene index searcher
-  * @param releaseSearcher a function for releasing the searcher
-  * @param indexSort       the sort of the index
-  * @param querySort       the sort in which the documents are going to be retrieved
-  * @param query           the query to be satisfied by the documents
-  * @param limit           the iteration page size
-  * @param fields          the names of the document fields to be loaded
+  * @param cursors   the searcher managers and pointers of the involved indexes
+  * @param indexSort the sort of the index
+  * @param querySort the sort in which the documents are going to be retrieved
+  * @param query     the query to be satisfied by the documents
+  * @param limit     the iteration page size
+  * @param fields    the names of the document fields to be loaded
   * @author Andres de la Pena `adelapena@stratio.com`
   */
 class DocumentIterator(
-    searcher: IndexSearcher,
-    releaseSearcher: () => Unit,
-    afterTerm: Option[Term],
+    cursors: List[(SearcherManager, Option[Term])],
     indexSort: Sort,
     querySort: Sort,
     query: Query,
@@ -49,66 +46,79 @@ class DocumentIterator(
 
   private[this] val pageSize = Math.min(limit, MAX_PAGE_SIZE) + 1
   private[this] val documents = new java.util.LinkedList[(Document, ScoreDoc)]
-
-  private[this] var afterOffset = 0
+  private[this] val indices = cursors.indices
+  private[this] val managers = cursors.map(_._1)
+  private[this] val searchers = managers.map(_.acquire())
+  private[this] val afterTerms = cursors.map(_._2)
+  private[this] val offsets = cursors.map(_ => 0).toArray
   private[this] var finished = false
   private[this] var closed = false
 
+  private[this] def releaseSearchers(): Unit =
+    indices.foreach(i => managers(i).release(searchers(i)))
+
   /** The sort of the query rewritten by the searcher. */
   private[this] val sort = try {
-    querySort.rewrite(searcher)
+    querySort.rewrite(searchers.head)
   } catch {
     case e: Exception =>
-      releaseSearcher()
+      releaseSearchers()
       throw new IndexException(e, s"Error rewriting sort $indexSort")
   }
 
-  /** The start after position. */
-  private[this] var after = try {
-    afterTerm.map(
-      term => {
-        val time = TimeCounter.start
-        val builder = new BooleanQuery.Builder
-        builder.add(new TermQuery(term), FILTER)
-        builder.add(query, MUST)
-        val scores = searcher.search(builder.build, 1, sort).scoreDocs
-        if (scores.nonEmpty) {
-          tracer.trace("Lucene index seeks last index position")
-          logger.debug(s"Start position found in $time")
-          scores.head
-        } else throw new IndexException("Last page position not found")
-      })
+  /** The start after positions. */
+  private[this] val afters = try {
+    indices.map(i => afterTerms(i).map(term => {
+      val time = TimeCounter.start
+      val builder = new BooleanQuery.Builder
+      builder.add(new TermQuery(term), FILTER)
+      builder.add(query, MUST)
+      val scores = searchers(i).search(builder.build, 1, sort).scoreDocs
+      if (scores.nonEmpty) {
+        tracer.trace("Lucene index seeks last index position")
+        logger.debug(s"Start position found in $time")
+        scores.head
+      } else throw new IndexException("Last page position not found")
+    })).toArray
   } catch {
     case e: Exception =>
-      releaseSearcher()
+      releaseSearchers()
       throw new IndexException(e, "Error while searching for the last page position")
   }
 
   private[this] def fetch() = {
     try {
-      val time = TimeCounter.start
+      val fetchTime = TimeCounter.start
 
-      val topDocs = if (afterTerm.isEmpty && canEarlyTerminate(sort, indexSort)) {
-        val fieldDoc = after.map(_.asInstanceOf[FieldDoc]).orNull
-        val collector = TopFieldCollector.create(sort, pageSize, fieldDoc, true, false, false)
-        val hits = afterOffset + pageSize
-        val earlyCollector = new EarlyTerminatingSortingCollector(collector, sort, hits, indexSort)
-        searcher.search(query, earlyCollector)
-        collector.topDocs
-      } else searcher.searchAfter(after.orNull, query, pageSize, sort)
+      val fieldDocs = indices.map(i => {
+        val afterTerm = afterTerms(i)
+        if (afterTerm.isEmpty && canEarlyTerminate(sort, indexSort)) {
+          val fieldDoc = afters(i).map(_.asInstanceOf[FieldDoc]).orNull
+          val collect = TopFieldCollector.create(sort, pageSize, fieldDoc, true, false, false)
+          val hits = offsets(i) + pageSize
+          val earlyCollect = new EarlyTerminatingSortingCollector(collect, sort, hits, indexSort)
+          searchers(i).search(query, earlyCollect)
+          val topDocs = collect.topDocs
+          offsets(i) += topDocs.scoreDocs.length
+          topDocs
+        } else searchers(i).searchAfter(afters(i).orNull, query, pageSize, sort, false, false)
+      }).toArray
 
-      val scoreDocs = topDocs.scoreDocs
+      // Merge partitions results
+      val scoreDocs = TopDocs.merge(sort, pageSize, fieldDocs).scoreDocs
+
       val numFetched = scoreDocs.length
-      afterOffset += numFetched
       finished = numFetched < pageSize
+
       for (scoreDoc <- scoreDocs) {
-        after = Some(scoreDoc)
-        val document = searcher.doc(scoreDoc.doc, fields)
+        val shard = scoreDoc.shardIndex
+        afters(shard) = Some(scoreDoc)
+        val document = searchers(shard).doc(scoreDoc.doc, fields)
         documents.add((document, scoreDoc))
       }
 
       tracer.trace(s"Lucene index fetches $numFetched documents")
-      logger.debug(s"Page fetched with $numFetched documents in $time")
+      logger.debug(s"Page fetched with $numFetched documents in $fetchTime")
 
     } catch {
       case e: Exception =>
@@ -143,8 +153,8 @@ class DocumentIterator(
   }
 
   /** Closes the [[IndexSearcher]] and any other resources */
-  override def close() = {
-    if (!closed) try releaseSearcher() finally closed = true
+  override def close(): Unit = {
+    if (!closed) try releaseSearchers() finally closed = true
   }
 
 }
