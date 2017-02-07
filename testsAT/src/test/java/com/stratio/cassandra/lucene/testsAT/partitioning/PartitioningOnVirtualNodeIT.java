a27763a9b3bd7460eacd250ac2c28c614fda1d3b
edu <eduardoalonso@stratio.com>
2017-02-07 17:54:28 +0100

    Refactor PartitioningOnVirtualNodeITest.

--- testsAT/src/test/java/com/stratio/cassandra/lucene/testsAT/partitioning/PartitioningOnVirtualNodeIT.java
+++ testsAT/src/test/java/com/stratio/cassandra/lucene/testsAT/partitioning/PartitioningOnVirtualNodeIT.java
@@ -47,78 +47,66 @@ public class PartitioningOnVirtualNodeIT extends BaseIT {
         numTokens = tokens.get(0).size();
     }
 
-    public static void createTestAndDeleteContext(int vNodesPerPartition) {
-        builder("virtual_nodes_partitioning")
-                .withTable("test")
-                .withIndexName("idx")
-                .withColumn("pk1", "int")
-                .withColumn("pk2", "int")
-                .withColumn("ck", "int")
-                .withColumn("rc", "int")
-                .withIndexColumn(null)
-                .withPartitionKey("pk1", "pk2")
-                .withClusteringKey("ck")
-                .withPartitioner(new Partitioner.OnVirtualNode(vNodesPerPartition))
-                .build()
-                .createKeyspace()
-                .createTable()
-                .createIndex()
-                .insert(new String[]{"pk1", "pk2", "ck", "rc"}, new Object[]{0, 0, 0, 0})
-                .insert(new String[]{"pk1", "pk2", "ck", "rc"}, new Object[]{0, 0, 1, 0})
-                .insert(new String[]{"pk1", "pk2", "ck", "rc"}, new Object[]{0, 1, 0, 0})
-                .insert(new String[]{"pk1", "pk2", "ck", "rc"}, new Object[]{0, 1, 1, 0})
-                .insert(new String[]{"pk1", "pk2", "ck", "rc"}, new Object[]{1, 0, 0, 0})
-                .insert(new String[]{"pk1", "pk2", "ck", "rc"}, new Object[]{1, 0, 1, 0})
-                .insert(new String[]{"pk1", "pk2", "ck", "rc"}, new Object[]{1, 1, 0, 0})
-                .insert(new String[]{"pk1", "pk2", "ck", "rc"}, new Object[]{1, 1, 1, 0})
-                .refresh()
-                .filter(all()).fetchSize(2).check(8)
-                .filter(all()).andEq("pk1", 0).andEq("pk2", 0).fetchSize(2).check(2)
-                .filter(all()).andEq("pk1", 0).andEq("pk2", 0).andEq("ck", 0).fetchSize(2).check(1)
-                .filter(all()).andEq("pk1", 0).allowFiltering(true).fetchSize(2).check(4)
-                .filter(all()).andEq("pk1", 1).allowFiltering(true).fetchSize(2).check(4)
-                .dropIndex()
-                .dropKeyspace();
+    public static void test(int vNodesPerPartition) {
+        if ((numTokens == 1) && (vNodesPerPartition >1)){
+            logger.debug("[IGNORED] vnode test because target cassandra cluster is not configured with virtual tokens.");
+        } else {
+            builder("virtual_nodes_partitioning")
+                    .withTable("test")
+                    .withIndexName("idx")
+                    .withColumn("pk1", "int")
+                    .withColumn("pk2", "int")
+                    .withColumn("ck", "int")
+                    .withColumn("rc", "int")
+                    .withIndexColumn(null)
+                    .withPartitionKey("pk1", "pk2")
+                    .withClusteringKey("ck")
+                    .withPartitioner(new Partitioner.OnVirtualNode(vNodesPerPartition))
+                    .build()
+                    .createKeyspace()
+                    .createTable()
+                    .createIndex()
+                    .insert(new String[]{"pk1", "pk2", "ck", "rc"}, new Object[]{0, 0, 0, 0})
+                    .insert(new String[]{"pk1", "pk2", "ck", "rc"}, new Object[]{0, 0, 1, 0})
+                    .insert(new String[]{"pk1", "pk2", "ck", "rc"}, new Object[]{0, 1, 0, 0})
+                    .insert(new String[]{"pk1", "pk2", "ck", "rc"}, new Object[]{0, 1, 1, 0})
+                    .insert(new String[]{"pk1", "pk2", "ck", "rc"}, new Object[]{1, 0, 0, 0})
+                    .insert(new String[]{"pk1", "pk2", "ck", "rc"}, new Object[]{1, 0, 1, 0})
+                    .insert(new String[]{"pk1", "pk2", "ck", "rc"}, new Object[]{1, 1, 0, 0})
+                    .insert(new String[]{"pk1", "pk2", "ck", "rc"}, new Object[]{1, 1, 1, 0})
+                    .refresh()
+                    .filter(all()).fetchSize(2).check(8)
+                    .filter(all()).andEq("pk1", 0).andEq("pk2", 0).fetchSize(2).check(2)
+                    .filter(all()).andEq("pk1", 0).andEq("pk2", 0).andEq("ck", 0).fetchSize(2).check(1)
+                    .filter(all()).andEq("pk1", 0).allowFiltering(true).fetchSize(2).check(4)
+                    .filter(all()).andEq("pk1", 1).allowFiltering(true).fetchSize(2).check(4)
+                    .dropIndex()
+                    .dropKeyspace();
+        }
     }
 
     @Test
     public void testVNodesWithOneVNodePerPartition() {
-        createTestAndDeleteContext(1);
+        test(1);
     }
 
     @Test
     public void testVNodesWithNumTokensVNodePerPartition() {
-        if (numTokens == 1) {
-            logger.debug("[IGNORED] testVNodesWithNumTokensVNodePerPartition because target cassandra cluster is not configured with virtual tokens.");
-        } else {
-            createTestAndDeleteContext(numTokens);
-        }
+        test(numTokens);
     }
 
     @Test
     public void testVNodesWithOneAndAHalfNumTokensVNodePerPartition() {
-        if (numTokens == 1) {
-            logger.debug("[IGNORED] testVNodesWithOneAndAHalfNumTokensVNodePerPartition because target cassandra cluster is not configured with virtual tokens.");
-        } else {
-            createTestAndDeleteContext(Math.round(numTokens * 1.5f));
-        }
+        test(Math.round(numTokens * 1.5f));
     }
 
     @Test
     public void testVNodesWithFloorNumTokensDiv2VNodePerPartition() {
-        if (numTokens == 1) {
-            logger.debug("[IGNORED] testVNodesWithFloorNumTokensDiv2VNodePerPartition because target cassandra cluster is not configured with virtual tokens.");
-        } else {
-            createTestAndDeleteContext(Math.floorDiv(Math.round(numTokens), 2));
-        }
+        test(Math.floorDiv(Math.round(numTokens), 2));
     }
 
     @Test
     public void testVNodesWithFloorNumTokensDiv3VNodePerPartition() {
-        if (numTokens == 1) {
-            logger.debug("[IGNORED] testVNodesWithFloorNumTokensDiv3VNodePerPartition because target cassandra cluster is not configured with virtual tokens.");
-        } else {
-            createTestAndDeleteContext(Math.floorDiv(Math.round(numTokens), 3));
-        }
+        test(Math.floorDiv(Math.round(numTokens), 3));
     }
 }
