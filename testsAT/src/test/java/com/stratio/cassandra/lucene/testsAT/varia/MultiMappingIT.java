f7bc28d3e28e823725ac758eee26812cc1124209
edu <eduardoalonso@stratio.com>
2017-04-05 11:05:04 +0200

    Added needed runwith tag in testsAT.

--- testsAT/src/test/java/com/stratio/cassandra/lucene/testsAT/varia/MultiMappingIT.java
+++ testsAT/src/test/java/com/stratio/cassandra/lucene/testsAT/varia/MultiMappingIT.java
@@ -20,12 +20,15 @@ import com.stratio.cassandra.lucene.testsAT.util.CassandraUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static com.stratio.cassandra.lucene.builder.Builder.*;
 
 /**
  * @author Andres de la Pena <adelapena@stratio.com>
  */
+@RunWith(JUnit4.class)
 public class MultiMappingIT extends BaseIT {
 
     private static CassandraUtils utils;
